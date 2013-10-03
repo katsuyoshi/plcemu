5342aeb94e3abc825d0d660f5f99632e353b58ce
Katsuyoshi Ito <kito@itosoft.com>
2013-10-04 00:12:17 +0900

    add q series

---
+++ mitsubishi/q/qbin.rb
@@ -0,0 +1,153 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+# Usage: ./qbin.rb
+require 'webrick'
+require './qdevice'
+
+DEFAULT_PORT = 5010
+DEFAULT_STATUS_FROM_PLC = "D7989"
+
+
+class Q < WEBrick::GenericServer
+  
+  HEADER_LENGTH = 11
+  
+  def initialize config = {}, default = WEBrick::Config::General
+    super
+    @device_dict = {}
+    @device_dict[DEFAULT_STATUS_FROM_PLC] = 1
+  end
+  
+  def run(sock)
+    done = false
+    buf = []
+    while true
+      c = sock.getc
+      break if c.nil? || c == ""
+      buf << c
+      next if buf.length < 15
+
+      case buf[HEADER_LENGTH, 4]
+      
+      # read bit device
+      when [0x01, 0x04, 0x01, 0x00]
+        next if buf.length < HEADER_LENGTH + 10
+        count = short_value buf[HEADER_LENGTH + 8, 2]
+        d = QDevice.new buf[HEADER_LENGTH + 4, 4]
+        res = [[0xd0, 0x00,  0x00,  0xff,  0xff, 0x03,  0x00],  [0x02, 0x00],  [0x00, 0x00]].flatten
+        res[7] = 2 + (count + 1) / 2
+        count.times do |i|
+          v = @device_dict[d.name] || 0
+          if i % 2 == 0
+            res << (v == 0 ? 0 : 0x10)
+          else
+            res[-1] |= (v == 0 ? 0 : 1)
+          end
+          d = d.next_device
+        end
+        sock.write res.pack("c*")
+        done = true
+      
+      # write bit device
+      when [0x01, 0x14, 0x01, 0x00]
+        next if buf.length < HEADER_LENGTH + 10
+        count = short_value buf[HEADER_LENGTH + 8, 2]
+        next if buf.length < HEADER_LENGTH + 10 + (count + 1) / 2
+        d = QDevice.new buf[HEADER_LENGTH + 4, 4]
+        count.times do |i|
+          index = HEADER_LENGTH + 10 + i / 2
+          if i % 2 == 0
+            @device_dict[d.name] = buf[index] & 0x10 == 0 ? 0 : 1
+          else
+            @device_dict[d.name] = buf[index] & 0x1 == 0 ? 0 : 1
+          end
+        end
+        res = [[0xd0, 0x00,  0x00,  0xff,  0xff, 0x03,  0x00],  [0x02, 0x00],  [0x00, 0x00]].flatten
+        sock.write res.pack("c*")
+        done = true
+      
+      # read word device
+      when [0x01, 0x04, 0x00, 0x00]
+        next if buf.length < HEADER_LENGTH + 10
+        count = short_value buf[HEADER_LENGTH + 8, 2]
+        d = QDevice.new buf[HEADER_LENGTH + 4, 4]
+        res = [[0xd0, 0x00,  0x00,  0xff,  0xff, 0x03,  0x00],  [0x02, 0x00],  [0x00, 0x00]].flatten
+        res[7] = 2 + count * 2
+        count.times do |i|
+          v = 0
+          if d.bit_device?
+            16.times do |i|
+              v |= (((@device_dict[d.name] || 0) == 0 ? 0 : 1) << i)
+              d = d.next_device
+            end
+          else
+            v = @device_dict[d.name] || 0
+            d = d.next_device
+          end
+          res << (v & 0xff); v >>= 8
+          res << (v & 0xff)
+        end
+        sock.write res.pack("c*")
+        done = true
+      
+      # write word device
+      when [0x01, 0x14, 0x00, 0x00]
+        next if buf.length < HEADER_LENGTH + 10
+        count = short_value buf[HEADER_LENGTH + 8, 2]
+        next if buf.length < HEADER_LENGTH + 10 + count * 2
+        d = QDevice.new buf[HEADER_LENGTH + 4, 4]
+        count.times do |i|
+          index = HEADER_LENGTH + 10 + i * 2
+          v = buf[index] || (buf[index + 1] << 8)
+          if d.bit_device?
+            16.times do |i|
+              @device_dict[d.name] = v & (1 << i) ? 1 : 0
+              d = d.next_device
+            end
+          else
+            @device_dict[d.name] = v
+            d = d.next_device
+          end
+        end
+        res = [[0xd0, 0x00,  0x00,  0xff,  0xff, 0x03,  0x00],  [0x02, 0x00],  [0x00, 0x00]].flatten
+        sock.write res.pack("c*")
+        done = true
+
+      # remote run
+      when 0x13
+        @device_dict["M8000"] = 1
+        res = [buf[0] | 0x80, 0]
+        sock.write res.pack("c*")
+        done = true
+      
+      # remote stop
+      when 0x14
+        @device_dict["M8000"] = 0
+        res = [buf[0] | 0x80, 0]
+        sock.write res.pack("c*")
+        done = true
+      
+      end
+      
+      if done
+        puts ">> #{buf.map{|c| ("0" + c.to_s(16).upcase)[-2, 2]}}"
+        puts "<< #{res.map{|c| ("0" + c.to_s(16).upcase)[-2, 2]}}"
+p @device_dict
+        buf = []
+        done = false
+      end
+      
+    end
+    puts "Close"
+  end
+  
+  def short_value a
+    a[1] << 8 | a[0]
+  end
+  
+end
+
+server = Q.new(:Port => DEFAULT_PORT)
+trap(:INT) { server.shutdown }
+server.start
+
---
+++ mitsubishi/q/qdevice.rb
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+class QDevice
+  attr_reader :suffix, :number
+  def initialize a, b = nil
+    case a
+    when Array
+      case a.size
+      when 4
+        @suffix = suffix_for_code(a[3])
+        @number = ((a[2] << 8 | a[1]) << 8) | a[0]
+      end
+    when String
+      if b
+        @suffix = a
+        @number = b
+      else
+        if a.length == 12
+          @suffix = [a[0,2].to_i(16), a[2,2].to_i(16)].pack "c*"
+          @suffix.strip!
+          @number = a[4,8].to_i(16)
+        elsif /(X|Y)(.+)/ =~ a
+          @suffix = $1
+          @number = $2.to_i(p_adic_number)
+        else
+          /(M|L|S|B|F|T|C|D|W|R)(.+)/ =~ a
+          @suffix = $1
+          @number = $2.to_i(p_adic_number)
+        end
+      end
+    end
+  end
+  
+  def p_adic_number
+    case @suffix
+    when "X", "Y", "B", "W", "SB", "SW", "DX", "DY", "ZR"
+      16
+    else
+      10
+    end
+  end
+  
+  def name
+    @suffix + @number.to_s(p_adic_number).upcase
+  end
+  
+  def next_device
+    d = self.class.new @suffix, @number + 1
+    d
+  end
+  
+  def bit_device?
+    case @suffix
+    when "SM", "X", "Y", "M", "L", "F", "V", "B",
+         "TS", "TC", "SS", "SC","CS", "CC", "SB", "S", "DX", "DY"
+      true
+    else
+      false
+    end
+  end
+  
+  def suffix_for_code code
+    @@suffixes ||= %w(SM SD X Y M L F V B D W TS TC TN SS SC SN CS CC CN SB SW S DX DY Z R ZR)
+    @@suffix_codes ||= [0x91, 0xa9, 0x9c, 0x9d, 0x90, 0x92, 0x93, 0x94, 0xa0, 0xa8, 0xb4, 0xc1, 0xc0, 0xc2, 0xc7, 0xc6, 0xc8, 0xc4, 0xc3, 0xc5, 0xa1, 0xb5, 0x98, 0xa2, 0xa3, 0xcc ,0xaf, 0xb0]
+      
+      index = @@suffix_codes.index code
+      index == -1 ? nil : @@suffixes[index]
+  end
+  
+end
+
+=begin
+d = FxDevice.new [0, 0, 0, 0, 32, 68]
+p d.name
+p d.next_device.name
+
+p FxDevice.new "D", 0
+p FxDevice.new "D0"
+p FxDevice.new "X10"
+p FxDevice.new("X10").name
+p FxDevice.new "4D2000000064".unpack("c*")
+p FxDevice.new "4D2000000064"
+p FxDevice.new "582000000011"
+p FxDevice.new("582000000011").name
+=end
