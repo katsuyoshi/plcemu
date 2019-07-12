672dc29854877bb717eda45a038285a512b12b6f
Katsuyoshi Ito <kito@itosoft.com>
2019-07-12 10:24:28 +0900

    fix to create next bit device

--- omron/fins/finsdevice.rb
+++ omron/fins/finsdevice.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class FinsDevice
   attr_reader :suffix, :number, :bit, :bit_access
-  
+
   def initialize a, b = nil
     case a
     when Array
@@ -21,7 +21,7 @@ class FinsDevice
         @number = to_int a[1, 2]
         @bit = to_int a[3, 1]
       end
-      
+
     when String
       if b
         @suffix = a
@@ -33,11 +33,11 @@ class FinsDevice
       end
     end
   end
-  
+
   def p_adic_number
     10
   end
-  
+
   def name
     n = @suffix + @number.to_s(p_adic_number).upcase
     if self.bit_access
@@ -45,24 +45,25 @@ class FinsDevice
     end
     n
   end
-  
+
   def channel_device
     self.class.new @suffix + @number.to_s(p_adic_number).upcase
   end
-  
+
   def next_device
     if self.bit_access
       bit = @bit += 1
+      number = @number
       if bit >= 16
         bit -= 16
-        number = @number + 1
+        number += 1
       end
       return self.class.new [@suffix, number, bit]
     else
       self.class.new @suffix, @number + 1
     end
   end
-  
+
   def to_suffix code
     s = { 0x30 => "", 0x31 => "W", 0x32 => "H", 0x33 => "A", 0x09 => "T", 0x2 => "D", 0x0a => "E", 0x06 => "TK"}[code]
     if s
@@ -72,7 +73,7 @@ class FinsDevice
     @bit_access = false
     { 0xB0 => "", 0xB1 => "W", 0xB2 => "H", 0xB3 => "A", 0x89 => "T", 0x82 => "D", 0x98 => "E", 0xbc => "DR"}[code]
   end
-  
+
   def to_int a
     v = 0
     a.each do |e|
@@ -81,7 +82,7 @@ class FinsDevice
     end
     v
   end
-  
+
   def bit_device?
     case @suffix
     when "X", "Y", "I", "E", "L", "M", "TU", "CU"
